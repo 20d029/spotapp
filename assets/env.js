3f51bc3c34cc02c4e59c7bc3b7250dc2c57ed824
20d029 <93080928+20d029@users.noreply.github.com>
2021-12-27 11:55:21 +0900

    Create env.js

--- assets/env.js
+++ assets/env.js
@@ -1,2 +1,2 @@
-SSID='20d029'
+SSID='https://sheets.googleapis.com/v4/spreadsheets/1gSQ2YYr6n-BBxtHspMPluQ2j9MfLd4U8lYJTuRAbugI/values/okazaki!A2:K1000?key=AIzaSyD_IOHRq23QuvGfO-N_LYKDyDs9-27v1ek'
 SNAME=okazaki!A2:K1000
